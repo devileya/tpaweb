56f7ac99d33c6e9aabbef069cebe39b9fde801b4
devileya <arif14si@mahasiswa.pcr.ac.id>
2017-06-19 11:38:33 +0700

    Update koneksi.php

--- kuis/koneksi.php
+++ kuis/koneksi.php
@@ -1,4 +1,4 @@
 <?php
-$koneksi = mysql_connect('localhost','root','') or die(mysql_error());
+$koneksi = mysql_connect('tpawebserver.mysql.database.azure.com','tpaweb@tpawebserver','@gakjelas12') or die(mysql_error());
 $db = mysql_select_db('tpa') or die (mysql_error());
 ?>
