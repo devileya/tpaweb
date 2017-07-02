54b840799d6c9d0e9696b4befd74165eb30e0392
devileya <arif14si@mahasiswa.pcr.ac.id>
2017-07-03 01:23:44 +0700

    Update update.php

--- kuis/update.php
+++ kuis/update.php
@@ -3,9 +3,9 @@
     include "koneksi.php";
     $response = array();
 
-    if (isset($_POST['id']) && isset($_POST['nama']) && isset($_POST['alamat']) && isset($_POST['telpon']) && isset($_POST['username'])  && isset($_POST['password'])) {
+    if (isset($_POST['sekolah']) && isset($_POST['nama']) && isset($_POST['alamat']) && isset($_POST['telpon']) && isset($_POST['username'])  && isset($_POST['password'])) {
 
-        $id = $_POST['id'];
+        $sekolah = $_POST['sekolah'];
         $nama = $_POST['nama'];
         $alamat = $_POST['alamat'];
         $telpon = $_POST['telpon'];
@@ -13,7 +13,7 @@
         $password = $_POST['password'];
         
 
-$query =   "UPDATE tbl_member SET nama='$nama', alamat='$alamat', telpon='$telpon' username='$username', password='$password' WHERE id='$id'";     
+$query =   "UPDATE tbl_member SET nama='$nama', alamat='$alamat', telpon='$telpon' sekolah='$sekolah', password='$password' WHERE username='$username'";     
  $result = mysql_query($query);
     }
 
